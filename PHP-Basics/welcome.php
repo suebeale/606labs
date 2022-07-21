3e8d43c6e2a07c65d60106b1e825d55e79630828
Sue Pearson <suebeale@gmail.com>
2022-07-22 09:15:22 +1200

    update if-else commit

--- PHP-Basics/welcome.php
+++ PHP-Basics/welcome.php
@@ -12,6 +12,13 @@ $myCourse = 'COMP606';
 
 # printing the txt to the screen
 echo 'Welcome to ' . $myCourse . '. You tutor is ' . $myName;
+
+if ($myCourse == 'COMP606') {
+    echo 'You are enrolled in the right course - COMP606';
+} else {
+    echo 'You are in the wrong course';
+}
+
 exit;
 ?>
 Hi, I'm only HTML
