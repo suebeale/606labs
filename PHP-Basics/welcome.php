fdc4918e5bfd51085495f7947f21d8564de639da
Sue Pearson <suebeale@gmail.com>
2022-07-22 08:36:26 +1200

    initial commit

---
+++ PHP-Basics/welcome.php
@@ -0,0 +1,24 @@
+<?php
+echo "Welcome to XAMPP PHP training<br />";
+
+$myName = 'Sue Pearson';
+$myCourse = 'COMP606';
+
+echo 'Welcome to ' . $myCourse . '. You tutor is ' . $myName;
+exit;
+?>
+Hi, I'm only HTML
+
+/*
+Learning
+
+Variables
+https://www.w3schools.com/php/php_variables.asp
+
+Printing to screen & Concatenation
+https://www.w3schools.com/php/php_echo_print.asp
+
+all PHP tutorials
+https://www.w3schools.com/php/default.asp
+
+*/
