bdd8d0b1972538a66e18771a633b079a794084f4
Sue Pearson <suebeale@gmail.com>
2022-07-29 15:02:03 +1200

    adding array files

---
+++ Arrays/array-count.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$cars = array("Volvo", "BMW", "Toyota","Masda");
+
+// . is for concatenation
+echo "I have " . count($cars) . " cars";
+?>
+
+</body>
+</html>
